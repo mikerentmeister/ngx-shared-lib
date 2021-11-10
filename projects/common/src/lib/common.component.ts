e65d9a386b13930df5ce5ad4632142702fcaf68e
Michael Rentmeister <MRentmeister@experlogix.com>
2021-11-09 20:52:15 -0700

    Initial commit

---
+++ projects/common/src/lib/common.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'exp-common',
+  template: `
+    <p>
+      common works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class CommonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
